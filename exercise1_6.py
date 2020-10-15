4bd82fa6910c7363390325affea03a77acb848e8
ahmettoktasdot <67964367+ahmettoktasdot@users.noreply.github.com>
2020-10-15 12:49:06 +0300

    Update exercise1_6.py

--- exercise1_6.py
+++ exercise1_6.py
@@ -2,7 +2,7 @@ def cube(number):
     return (number*number*number)
 
 def by_Three (number):
-    if ((number % 3) == 0):
+    if (number % 3) == 0:
         return cube(number)
     else:
         return 'false'
